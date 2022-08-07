792e6f9080a4563b42dc5abbe18333e2142b364c
kisslune <yuxiang.lei@student.uts.edu.au>
2022-08-07 01:47:07 -0700

    builder shells

--- build.sh
+++ build.sh
@@ -6,12 +6,28 @@
 #########
 # Please change the following home directories of your LLVM builds
 ########
-LLVMRELEASE=$LLVM_OBJ
-LLVMDEBUG=$LLVM_OBJ
 
-export PATH=$LLVM_DIR/bin:$PATH
+# Install SVF if not exist
+if [ -z "$SVF_DIR" ]; then
+  # Download SVF
+  npm i --silent svf-lib --prefix ${HOME}
+  # Set environmental parameters
+  install_path=$(npm root)
+  LLVMHome="llvm-13.0.0.obj"
+  Z3Home="z3.obj"
+  export LLVM_DIR=$install_path/$LLVMHome
+  export Z3_DIR=$install_path/$Z3Home
+  export PATH=$LLVM_DIR/bin:$PATH
+  echo "export LLVM_DIR=$install_path/$LLVMHome" >>~/.bashrc
+  echo "export Z3_DIR=$install_path/$Z3Home" >>~/.bashrc
+  export SVF_DIR=$install_path/SVF/
 
+  echo "LLVM_DIR="$LLVM_DIR
+  echo "SVF_DIR="$SVF_DIR
+  echo "Z3_DIR="$Z3_DIR
+fi
 
+# Build POCR
 if [[ $1 == 'debug' ]]
 then
 Type='Debug'
@@ -31,8 +47,6 @@ cmake -D CMAKE_BUILD_TYPE:STRING=Debug ../
 else
 cmake ../
 fi
-
-
 
 make -j4
 
