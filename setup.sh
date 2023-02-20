7219e943db96bfb2c6f54153ddf526300f9e2430
kisslune <youthyoungray@gmail.com>
2023-02-19 19:26:46 -0800

    dev

--- setup.sh
+++ setup.sh
@@ -1,35 +1,6 @@
 #!/usr/bin/env bash
-echo "Setting up environment for PTA"
+echo "Setting up environment for POCR"
 
-
-#########
-# Please change LLVM_OBJ_ROOT before using it
-########
-
-export LLVM_OBJ_ROOT=$LLVM_OBJ
-
-export PATH=$LLVM_OBJ_ROOT/bin:$PATH
-export LLVM_DIR=$LLVM_OBJ_ROOT
-export LLVMOPT=opt
-export CLANG=$LLVM_OBJ_ROOT/bin/clang
-export CLANGCPP=$LLVM_OBJ_ROOT/bin/clang++
-export LLVMDIS=llvm-dis
-export LLVMLLC=llc
-
-
-##############check what os we have
-PLATFORM='unknown'
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-export PLATFORM='linux'
-elif [[ "$unamestr" == 'Darwin' ]]; then
-export PLATFORM='darwin'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
-export PLATFORM='freebsd'
-fi
-
-
-#########PATH FOR PTA##############
 export PTAHOME=`pwd`
 if [[ $1 == 'debug' ]]
 then
@@ -38,17 +9,4 @@ else
 PTAOBJTY='Release'
 fi
 Build=$PTAOBJTY'-build'
-export PTABIN=$PTAHOME/$Build/bin
-export PTALIB=$PTAHOME/$Build/lib
-export PTARTLIB=$PTAHOME/lib/RuntimeLib
-export PATH=$PTABIN:$PATH
-
-
-### for mac 10.10.1###
-rm -rf $PTALIB/liblib*
-for file in $(find $PTALIB -name "*.dylib")
-do
-    basefilename=`basename $file`
-    newfile=`echo $basefilename | sed s/lib/liblib/`
-    ln -s $PTALIB/$basefilename $PTALIB/$newfile
-done
+export PATH=$PTAHOME/$Build/bin:$PATH
